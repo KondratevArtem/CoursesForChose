810895e321ea3bd23d6d4acd9b8d8fa993d95756
Kondratev Artem <kondrartyom2@gmail.com>
2020-05-20 22:18:25 +0300

    Two programs: first - on java, second - python

---
+++ JavaProgram.java
@@ -0,0 +1,11 @@
+public class JavaProgram {
+    public static void main(String[] args) {
+        String name = "Artem";
+        String lastName = "Kondratev";
+        String group = "11-901";
+        String city = "Kazan";
+        String hobby = "Creating rc airplanes, playing guitar, programming";
+        System.out.println(name + "\n" + lastName + "\n" + group + "\n" +
+                city + "\n" + hobby);
+    }
+}
---
+++ PythonProgram.py
@@ -0,0 +1,7 @@
+name = 'Artem'
+last_name = 'Kondratev'
+group = '11-901'
+city = 'Kazan'
+hobby = 'Creating rc airplanes, playing guitar, programming'
+print(name + '\n' + last_name + '\n' + group + '\n' +
+      city + '\n' + hobby)
